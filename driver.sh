f2392aa2af52785242114ea74ecef7cf1272807c
Nick Desaulniers <nick.desaulniers@gmail.com>
2018-10-30 01:15:14 -0700

    driver: add qemu boot test

--- driver.sh
+++ driver.sh
@@ -8,6 +8,9 @@ check_dependencies() {
   test -x `which gcc`
   test -x `which aarch64-linux-gnu-as`
   test -x `which aarch64-linux-gnu-ld`
+  test -x `which qemu-system-aarch64`
+  test -x `which timeout`
+  test -x `which unbuffer`
 
   set +e
 }
@@ -35,7 +38,7 @@ build_linux() {
   export CROSS_COMPILE=aarch64-linux-gnu-
   # TODO: do we really want to be building the kernel from scratch every time?
   # Rerunning ninja above will not rebuild unless source files change.
-  make CC=$clang mrproper
+  #make CC=$clang mrproper
   make CC=$clang defconfig
   make CC=$clang -j`nproc`
   cd -
@@ -54,7 +57,26 @@ build_root() {
   rm -rf buildroot/overlays
 }
 
+boot_qemu() {
+  local kernel_image=linux/arch/arm64/boot/Image.gz
+  local rootfs=buildroot/output/images/rootfs.ext4
+  # for the rest of the script, particularly qemu
+  set -e
+  test -e $kernel_image
+  test -e $rootfs
+  timeout 1m unbuffer qemu-system-aarch64 \
+    -machine virt \
+    -cpu cortex-a57 \
+    -m 512 \
+    -nographic \
+    -kernel $kernel_image \
+    -hda $rootfs \
+    -append "console=ttyAMA0 root=/dev/vda" \
+    -no-reboot
+}
+
 check_dependencies
 build_clang
 build_linux
 build_root
+boot_qemu

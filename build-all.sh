0eefc462313c016fe1bd0a86d0c1a6128cf06a7a
Nathan Chancellor <natechancellor@gmail.com>
2019-06-04 16:52:11 -0700

    build-all.sh: Use variables for color escape codes
    [skip ci]
    
    Requested-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- build-all.sh
+++ build-all.sh
@@ -15,6 +15,9 @@ function format_time() {(
 )}
 
 BLD_ALL_START=$(date +%s)
+GREEN="\033[01;32m"
+RED="\033[01;31m"
+RESET="\033[0m"
 
 echo
 grep "env:" .travis.yml | grep -v LLVM_VERSION | sed "s/.*env: //g" | while read -r ITEM; do (
@@ -30,9 +33,9 @@ grep "env:" .travis.yml | grep -v LLVM_VERSION | sed "s/.*env: //g" | while read
 
     echo -e "Running '${ARCH:+ARCH=${ARCH} }${LD:+LD=${LD} }${REPO:+REPO=${REPO} }./driver.sh'... \c"
     if ! ./driver.sh "${@}" &>/dev/null; then
-        echo -e "\033[01;31mFailed\033[0m\c"
+        echo -e "${RED}Failed${RESET}\c"
     else
-        echo -e "\033[01;32mSuccessful\033[0m\c"
+        echo -e "${GREEN}Successful${RESET}\c"
     fi
     echo " in $(format_time "${DRIVER_START}" "$(date +%s)")"
 ) done

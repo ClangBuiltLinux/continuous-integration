f2d875413f3945d581bd0bffcfc97821413494b2
Nathan Chancellor <natechancellor@gmail.com>
2019-07-08 19:41:27 -0700

    buildroot/rebuild.sh: Make it easier to rebuild all of the images
    Instead of having to rebuild the images one by one, a user can run:
    
    $ ./rebuild.sh all
    
    Additionally, multiple images can be rebuilt at once.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- buildroot/rebuild.sh
+++ buildroot/rebuild.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-# Takes the architecture as a parameter
-# Currently supported values: arm64
+# Takes a list of architectures to build images for as the parameter
 
 # Make sure we don't have any unset variables
 set -u
+
+# Generate list of configs to build
+CONFIGS=()
+while (( ${#} )); do
+    case ${1} in
+        all) for CONFIG in *.config; do CONFIGS+=( "../${CONFIG}" ); done ;;
+        arm64|arm|ppc32|ppc64le|x86_64) CONFIGS+=( "../${1}.config" ) ;;
+        *) echo "Unknown parameter '${1}', exiting!"; exit 1 ;;
+    esac
+    shift
+done
 
 # Download latest buildroot release
 BUILDROOT_VERSION=2019.02.3
@@ -13,40 +23,37 @@ if [[ -d src ]]; then
         git fetch origin ${BUILDROOT_VERSION}
         git checkout ${BUILDROOT_VERSION}
     fi
-
-    # Clean up artifacts from the last build
-    make clean
 else
     git clone -b ${BUILDROOT_VERSION} git://git.busybox.net/buildroot src
     cd src || exit 1
 fi
 
-# Use the config in the parent folder
-CONFIG=../${1}.config
-if [[ ! -f ${CONFIG} ]]; then
-    echo "${CONFIG} does not exist! Is your parameter correct?"
-    exit 1
-fi
-BR2_DEFCONFIG=${CONFIG} make defconfig
-if [[ -n ${EDITCONFIG:-} ]]; then
-    make menuconfig
-    make savedefconfig
-fi
+# Build the images for the architectures requested
+for CONFIG in "${CONFIGS[@]}"; do
+    # Clean up artifacts from the last build
+    make clean
 
-# Build images
-make -j"$(nproc)"
-
-# Make sure images folder exists
-IMAGES_FOLDER=../../images/${1}
-[[ ! -d ${IMAGES_FOLDER} ]] && mkdir -p "${IMAGES_FOLDER}"
-
-# Copy new images
-# Make sure images exist before moving them
-IMAGES=( "output/images/rootfs.cpio" "output/images/rootfs.ext4" )
-for IMAGE in "${IMAGES[@]}"; do
-    if [[ ! -f ${IMAGE} ]]; then
-        echo "${IMAGE} could not be found! Did the build error?"
-        exit 1
+    BR2_DEFCONFIG=${CONFIG} make defconfig
+    if [[ -n ${EDITCONFIG:-} ]]; then
+        make menuconfig
+        make savedefconfig
     fi
-    cp -v "${IMAGE}" "${IMAGES_FOLDER}"
+
+    # Build images
+    make -j"$(nproc)"
+
+    # Make sure images folder exists
+    IMAGES_FOLDER=../../images/$(basename "${CONFIG//.config}")
+    [[ ! -d ${IMAGES_FOLDER} ]] && mkdir -p "${IMAGES_FOLDER}"
+
+    # Copy new images
+    # Make sure images exist before moving them
+    IMAGES=( "output/images/rootfs.cpio" "output/images/rootfs.ext4" )
+    for IMAGE in "${IMAGES[@]}"; do
+        if [[ ! -f ${IMAGE} ]]; then
+            echo "${IMAGE} could not be found! Did the build error?"
+            exit 1
+        fi
+        cp -v "${IMAGE}" "${IMAGES_FOLDER}"
+    done
 done

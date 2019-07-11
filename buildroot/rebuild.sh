ae3585e61f71bddaf508dd27766d7d269551b023
Nathan Chancellor <natechancellor@gmail.com>
2019-07-11 11:09:31 -0700

    buildroot/rebuild.sh: Validate checksum of downloaded tarball
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- buildroot/rebuild.sh
+++ buildroot/rebuild.sh
@@ -3,7 +3,16 @@
 
 function download_br() {
     mkdir -p src
-    curl https://buildroot.org/downloads/buildroot-"${BUILDROOT_VERSION}".tar.gz | tar -xzf - -C src --strip-components=1
+    TARBALL=buildroot-${BUILDROOT_VERSION}.tar.gz
+    rm -rf "${TARBALL}"
+    curl -LO https://buildroot.org/downloads/"${TARBALL}"
+    SHA256=31a0dd7f5df75d20f6b74f0684918b643e5bf08d87dd5f9f02525852eecccf95
+    if [[ $(sha256sum "${TARBALL}" | cut -d ' ' -f 1) != ${SHA256} ]]; then
+        echo "Downloaded tarball's hash does not match known good one! Please try redownloading."
+        exit 1
+    fi
+    tar -xzf "${TARBALL}" -C src --strip-components=1
+    rm -rf "${TARBALL}"
 }
 
 # Make sure we don't have any unset variables

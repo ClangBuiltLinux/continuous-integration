74df124f08346421e46baf4a8d7ed084f954dc68
Nick Desaulniers <ndesaulniers@google.com>
2020-06-16 11:26:38 -0700

    boot test riscv linux-next
    had to rewire some of this, since we need finer grain control.

--- driver.sh
+++ driver.sh
@@ -124,14 +124,12 @@ setup_variables() {
         "riscv")
             config=defconfig
             make_target=vmlinux
-            using_qemu=false
             export CROSS_COMPILE=riscv64-linux-gnu-
             ;;
 
         "s390")
             config=defconfig
             make_target=bzImage
-            using_qemu=false
             export CROSS_COMPILE=s390x-linux-gnu-
 
             # llvm-objcopy: error: invalid output format: 'elf64-s390'
@@ -416,7 +414,7 @@ build_linux() {
 }
 
 boot_qemu() {
-    ${using_qemu:=true} || return 0
+    if [[ "${BOOT}" == "0" ]]; then return 0; fi
     ./boot-utils/boot-qemu.sh -a "${SUBARCH}" -k "${tree}" -t "${timeout:-2}"m
 }
 
